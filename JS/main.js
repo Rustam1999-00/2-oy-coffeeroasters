1a574bfe4746d1a3abf57328b49f7b48fc16032a
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-11-08 23:15:44 +0500

    init

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+var elform__contents = document.querySelector('.form__contents');
+var elpath__button = document.querySelector('.path__button');
+
+elpath__button.addEventListener('click', function(){
+    elform__contents.classList.toggle('open')
+});
+// 2-PAGE
+var elform__contents2 = document.querySelector('.form__contents2');
+var elpath__button2 = document.querySelector('.path__button2');
+
+elpath__button2.addEventListener('click', function(){
+    elform__contents2.classList.toggle('open')
+});
+// 3-PAGE
+var elform__contents3 = document.querySelector('.form__contents3');
+var elpath__button3 = document.querySelector('.path__button3');
+
+elpath__button3.addEventListener('click', function(){
+    elform__contents3.classList.toggle('open')
+});
+// 4-PAGE
+var elform__contents4 = document.querySelector('.form__contents4');
+var elpath__button4 = document.querySelector('.path__button4');
+
+elpath__button4.addEventListener('click', function(){
+    elform__contents4.classList.toggle('open')
+});
+// 5-PAGE
+var elform__contents5 = document.querySelector('.form__contents5');
+var elpath__button5 = document.querySelector('.path__button5');
+
+elpath__button5.addEventListener('click', function(){
+    elform__contents5.classList.toggle('open')
+});
